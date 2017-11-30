ee2bdf6f2a3fdcd39d1dd7591069c60e9ba9d310
Larry Reid <lcreid@jadesystems.ca>
2017-11-30 13:44:01 -0800

    Documentation.

--- lib/sgp4/sgp4/coordinates.rb
+++ lib/sgp4/sgp4/coordinates.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module Sgp4
+  # x, y, and z values. "Coordinates" is a bit of a misnomer,
+  # as the values can be position or velocity.
   class Coordinates
     def initialize(x, y, z)
       @x = x
@@ -8,10 +10,16 @@ module Sgp4
       @z = z
     end
 
+    # @return [String] a convenient string representation of the coordinates.
     def to_s
       "#{x} #{y} #{z}"
     end
 
-    attr_reader :x, :y, :z
+    # @return [Float] x value.
+    attr_reader :x
+    # @return [Float] y value.
+    attr_reader :y
+    # @return [Float] z value.
+    attr_reader :z
   end
 end
--- lib/sgp4/sgp4/sgp4.rb
+++ lib/sgp4/sgp4/sgp4.rb
@@ -1,13 +1,49 @@
 module Sgp4
+  # A propagator for near-earth and deep-space satellites.
   class Sgp4
+    # @!method calculate(since)
+    # Calculate the position and velocity of a satellite at a time since
+    # the time of the observation of the two-line element.
+    # {since} can be positive or negative.
+    # In general, the smaller the absolute value of since,
+    # the more accurate the propagation.
+    # Put another way,
+    # future predictions will be most accurate
+    # if you use the most up-to-date two-line elements you can find.
+    #
+    # The calculation is done based on either the SGP4 mathematical model
+    # or the SDP4 mathematical model. SGP4 is for near-earth satellites.
+    # SDP4 is for deep space satellites.
+    # The initialization of the propagator determines
+    # which mathematical model to use
+    # based on the two-line elements.
+    # @param since [Float] time in minutes since the two-line element
+    #   was observed (the "epoch").
+
+    # Initialize an SGP4/SDP4 propagator for a given two-line element.
+    # This includes determining whether to use the near-earth model SGP4,
+    # or the deep space model SDP4.
+    # @param tle [Array, Tle] an array of two strings in two-line element
+    #   format, or an already initialize Tle object.
     def initialize(tle)
       @tle = tle.is_a?(Tle) ? tle : Tle.new(tle)
     end
 
+    # @return [String] a convenient string representation of the position
+    #   and velocity of the satellite.
     def to_s
       puts "#{pos} #{vel}"
     end
 
-    attr_reader :tle, :pos, :vel
+    # @nodoc
+    attr_reader :tle
+
+    # @return [Coordinates] the position of the satellite
+    #   after {#calculate} has been called.
+    attr_reader :pos
+
+    # @return [Coordinates] the velocity of the satellite
+    #   after {#calculate} has been called.
+    attr_reader :vel
   end
 end
